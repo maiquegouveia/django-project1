3fd02317b4d9274590f9f661538fc759fa8c8349
Maique <maiquebusiness@gmail.com>
2023-01-28 01:05:29 -0300

    changes in the view home function

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Maique Gouveia',
-    })
+    return render(request, 'recipes/pages/home.html')
