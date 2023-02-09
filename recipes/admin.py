0b00c860f75ea7193b691388987a6e11429f967d
Maique <maiquebusiness@gmail.com>
2023-02-08 23:58:34 -0300

    Registered the application recipe models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . models import Category, Recipe
 
 # Register your models here.
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
